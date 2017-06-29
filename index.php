3a7f07a7202041dcbc0a02da774b7ff5ad7d7905
Florian Van Dinh <fvandinh@gmail.com>
2017-06-29 15:16:54 +0200

    Init
    Everything is set up

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Météo map</title>
+        <link rel="stylesheet" href="assets/css/style.css"/>
+    </head>
+    <body>
+        
+        <script src="assets/js/script.js"></script>
+    </body>
+</html>
